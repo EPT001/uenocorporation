f2520d7d0d48e5d503c77afeed9da18ace8d3f5e
EPT001 <155842366+EPT001@users.noreply.github.com>
2025-08-13 11:01:35 +0900

    Update urls.py

--- uenocorporation/urls.py
+++ uenocorporation/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('uenowebsite/', include('uenowebsite.urls')),
     path('admin/', admin.site.urls),
+    path('realestate.html', RedirectView.as_view(url='/', permanent=True)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
